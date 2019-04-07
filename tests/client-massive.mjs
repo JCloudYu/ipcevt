d689ed13f0d7ceee83a3d4b5a58c2900c45de1ae
JCloudYu <j.cloud.yu@gmail.com>
2019-04-07 17:01:00 +0800

    Add test to massive amount of data input

--- tests/client-massive.mjs
+++ tests/client-massive.mjs
@@ -5,8 +5,13 @@
 import {IPCEvtClient} from "../index.mjs";
 import crypto from "crypto";
 
+const TOTAL_SIZE = 1024 * 20;
 const id = crypto.randomBytes(12).toString('hex');
 const Client = new IPCEvtClient();
+
+const buff = crypto.randomBytes(2048).toString('base64');
+
+
 
 // Register the default argument serialization and deserialization logic
 Client._serializer	 = (input)=>{ return Buffer.from(JSON.stringify(input), 'utf8'); };
@@ -16,7 +21,21 @@ Client._deserializer = (input)=>{ return JSON.parse(input.toString('utf8')); };
 Client
 .on( 'connected', function() {
 	console.log(`Connected to event central! (${id})`);
-	this.emit('test-event', id, 'Hi! There!!!', {a:1, b:2, c:"123", d:Date.now()});
+	
+	let count = 1;
+	setTimeout(SEND_TEST_DATA, 0, this);
+	function SEND_TEST_DATA(inst) {
+		if ( count > TOTAL_SIZE ) return;
+	
+		
+		for(let i=1; i<=1024; i++) {
+			inst.emit( 'data', id, count, buff );
+			console.log( `Sending data #${count++}` );
+		}
+		
+		global.gc();
+		setTimeout(SEND_TEST_DATA, 5000, inst);
+	}
 })
 .on( 'disconnected', function() {
 	console.log(`Disconnected from event central! (${id})`);
@@ -24,8 +43,7 @@ Client
 .on( 'test-event', function(...args){
 	console.log(`Receiving [test-event]: `, ...args);
 })
-.on( 'data', function(rId , size, data){
-	const decoded = Buffer.from( data, 'base64' );
-	console.log(`Receiving data from ${rId} - ${size}!`, decoded.length === size);
-})
 .connect( 12334, 'localhost' );
+
+
+
